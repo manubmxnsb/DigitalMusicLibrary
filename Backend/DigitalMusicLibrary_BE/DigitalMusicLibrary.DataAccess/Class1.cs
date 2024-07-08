c8b4ffddea384f192ec5639adde2d6825bc4a586
Manuel Marginean <manuel.marginean@gmail.com>
2024-07-08 14:02:42 +0300

    Project Structure added

---
+++ Backend/DigitalMusicLibrary_BE/DigitalMusicLibrary.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DigitalMusicLibrary.DataAccess
+{
+	public class Class1
+	{
+
+	}
+}
---
+++ Backend/DigitalMusicLibrary_BE/DigitalMusicLibrary_BE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitalMusicLibrary_BE.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
